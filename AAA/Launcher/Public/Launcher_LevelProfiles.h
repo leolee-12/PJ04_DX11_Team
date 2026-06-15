a5a7db6a71ae0837259181295c730bafb1dda67b
Marb1e0817 <mabll0817@gmail.com>
2026-06-15 10:13:33 +0900

    path fix

--- AAA/Launcher/Public/Launcher_LevelProfiles.h
+++ AAA/Launcher/Public/Launcher_LevelProfiles.h
@@ -6,11 +6,11 @@ NS_BEGIN(Client)
 struct LAUNCHER_LEVEL_PROFILES final
 {
 	static constexpr const _tchar* LEVEL_FIRSTLOADING =
-		L"../../Resources/Level_Manifest/Level_FirstLoading.json";
+		L"../../Resources/YSH/Level_Manifest/Level_FirstLoading.json";
 	static constexpr const _tchar* LEVEL_LOADING =
-		L"../../Resources/Level_Manifest/Level_Loading.json";
+		L"../../Resources/YSH/Level_Manifest/Level_Loading.json";
 	static constexpr const _tchar* LEVEL_TEST =
-		L"../../Resources/Level_Manifest/Level_Test.json";
+		L"../../Resources/YSH/Level_Manifest/Level_Test.json";
 };
 
 NS_END
