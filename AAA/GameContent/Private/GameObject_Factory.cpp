246aff5a9f2ee9bd33bff7c727db7ab41a813d84
CHA\chaho <chahojun1004@gmail.com>
2026-07-01 16:12:04 +0900

    DespawnEffect 완성 + StarParticle 클래스 구현

--- AAA/GameContent/Private/GameObject_Factory.cpp
+++ AAA/GameContent/Private/GameObject_Factory.cpp
@@ -78,6 +78,7 @@
 #include "RockEffect.h"
 #include "RockFloorEffect.h"
 #include "SmokeParticle.h"
+#include "StarParticle.h"
 
 //sky
 #include "SkySphere.h"
@@ -719,8 +720,13 @@ void CGameObject_Factory::Register_Effect()
                 CSmokeParticle::Create(pDevice, pContext));
             TRY_ADD_PROTO(pProxy, iLevelIndex, TEXT("Prototype_Component_Model_SmokeSphereOriginal"),
                 CModel::Create(pDevice, pContext, MODEL::NONANIM, "../../Resources/Test/Effect/SmokeSphereOriginal/Model_SmokeSphereOriginal.ysh"));
-        )
-    );
+
+            TRY_ADD_PROTO(pProxy, iLevelIndex, CStarParticle::PROTOTYPE_TAG,
+                CStarParticle::Create(pDevice, pContext));
+            TRY_ADD_PROTO(pProxy, iLevelIndex, TEXT("Prototype_Component_Model_StarSmooth"),
+                CModel::Create(pDevice, pContext, MODEL::NONANIM, "../../Resources/CHJ/Effect/Star/Common_00_Common_StarSmooth2.ysh"));
+         
+        ));
 }
 
 void CGameObject_Factory::Register_MiniBoss()
---
+++ AAA/GameContent/Public/StarParticle.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "GameContent_Defines.h"
+
+#include "Effect_MeshParticle.h"
+
+NS_BEGIN(Client)
+
+class CStarParticle final : public CEffect_MeshParticle
+{
+	GENERATED_BODY(CStarParticle)
+
+public:
+	struct STAR_PARTICLE_DESC : public CEffect_MeshParticle::EFFECT_MESHPARTICLE_DESC
+	{
+
+	};
+
+	static constexpr const wchar_t* PROTOTYPE_TAG = L"Proto_StarParticle";
+
+private:
+	CStarParticle(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	CStarParticle(const CStarParticle& Prototype);
+	virtual ~CStarParticle() = default;
+
+public:
+	virtual HRESULT				Initialize_Prototype() override;
+	virtual HRESULT				Initialize(void* pArg) override;
+
+public:
+	virtual void				Priority_Update(_float fTimeDelta) override;
+	virtual void				Update(_float fTimeDelta) override;
+	virtual void				Late_Update(_float fTimeDelta) override;
+	virtual HRESULT				Render() override;
+	virtual void				Copy_PrototypeName(ENGINE_OBJECT_DATA* pOutData) override { pOutData->strPrototypeTag = PROTOTYPE_TAG; }
+
+public:
+	static CStarParticle*		Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual CGameObject*		Clone(void* pArg) override;
+private:
+	virtual void Free();
+};
+
+NS_END
