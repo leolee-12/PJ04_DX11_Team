46b76cdc8c36d29dd4e3d47888e7dd2c0427f865
Leo <leo.lee.dec@gmail.com>
2026-07-09 22:06:37 +0900

    [feat] IDeformable 추가

---
+++ AAA/GameContent/Public/Deformable.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "GameContent_Defines.h"
+
+NS_BEGIN(Client)
+class IDeformable;
+
+namespace EventTag
+{
+    inline constexpr const _tchar* Deform_Acquired = L"Deform.Acquired";
+}
+
+struct DEFORM_ACQUIRED_EVENT
+{
+    DEFORM_TYPE eType = { DEFORM_TYPE::NONE };
+    IDeformable* pSource = { nullptr };
+};
+
+enum class DEFORM_OBJECT_KIND
+{
+    MOBILE,           // 오브젝트가 커비 쪽으로 이동·회전하며 흡수됨
+    FIXED,            // 오브젝트는 고정, 커비가 오브젝트에 달라붙음
+};
+
+class IDeformable
+{
+protected:
+    virtual ~IDeformable() = default;
+
+public:
+    virtual DEFORM_TYPE         Get_DeformType() const = 0;
+    virtual DEFORM_OBJECT_KIND  Get_DeformKind() const = 0;
+
+    virtual _bool   Request_Deform() const = 0;
+    virtual HRESULT Begin_Deform(const _float4x4& AnchorWorld) = 0;
+    virtual void    End_Deform(const _float4x4& AnchorWorld) = 0;
+};
+
+NS_END
