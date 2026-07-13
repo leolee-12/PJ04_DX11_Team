6c158a1d215bb77a363698a87893b6ded1782b41
CHA\chaho <chahojun1004@gmail.com>
2026-07-13 20:07:09 +0900

    PR 전 임시 커밋

--- AAA/Launcher/Private/Boss_Stage1.cpp
+++ AAA/Launcher/Private/Boss_Stage1.cpp
@@ -63,7 +63,7 @@ HRESULT CBoss_Stage1::Initialize()
     if (FAILED(Ready_Lights()))
         return E_FAIL;
 
-    m_pGameInstance_Proxy->Play_BGM_Fade(L"K15_PreBoss.marker.wav", 3.f, 0.4f);     // 1ÃÊ Fade IN µé¾îº¸¸ç Æ©´×
+    m_pGameInstance_Proxy->Play_BGM_Fade(L"K15_PreBoss.marker.wav", 3.f, 0.15f);     // 1ÃÊ Fade IN µé¾îº¸¸ç Æ©´×
 
     m_pGameInstance_Proxy->Set_ShaderGlobal("g_fFogEnable", _float4(0.f, 0.f, 0.f, 0.f));
 
@@ -103,7 +103,7 @@ HRESULT CBoss_Stage1::Ready_Events()
     });
 
     Subscribe_Event(EventTag::BGMChange, [this](void* p) {
-        m_pGameInstance_Proxy->Play_BGM_Fade(L"K15_Boss1.marker.wav", 3.f, 0.5f);
+        m_pGameInstance_Proxy->Play_BGM_Fade(L"K15_Boss1.marker.wav", 3.f, 0.25f);
         });
 
     /*Subscribe_Event(EventTag::Cutscene_StageClear, [this](void* p) {
