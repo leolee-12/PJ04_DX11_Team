e7bf43217536e9bef55538c63488847e9b386d8b
ddoichaboom <chahojun1004@gmail.com>
2026-07-19 09:51:46 +0900

    SwordHitEffect 깡통 클래스 (잠정 보류 )

--- AAA/GameContent/Public/SwordHitEffect.h
+++ AAA/GameContent/Public/SwordHitEffect.h
@@ -1,5 +1,44 @@
 #pragma once
-class CSwordHitEffect
+
+#include "Effect_Container.h"
+
+NS_BEGIN(Client)
+
+class CSwordHitEffect final : public CEffect_Container
 {
+	GENERATED_BODY(CSwordHitEffect)
+
+public:
+	static constexpr const _tchar* PROTOTYPE_TAG = L"Proto_SwordHitEffect";
+
+private:
+	CSwordHitEffect(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	CSwordHitEffect(const CSwordHitEffect& Prototype);
+	virtual ~CSwordHitEffect() = default;
+
+protected:
+	virtual HRESULT Initialize_Prototype() override;
+	virtual HRESULT Initialize(void* pArg) override;
+
+public:
+	virtual void Priority_Update(_float fTimeDelta) override;
+	virtual void Update(_float fTimeDelta) override;
+	virtual void Late_Update(_float fTimeDelta) override;
+	virtual HRESULT Render() override;
+	virtual void Copy_PrototypeName(ENGINE_OBJECT_DATA* pOutData) override
+	{
+		pOutData->strPrototypeTag = PROTOTYPE_TAG;
+	}
+
+private:
+	HRESULT Ready_EffectPartObjects();
+
+public:
+	static CSwordHitEffect* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual CGameObject* Clone(void* pArg) override;
+
+private:
+	virtual void Free() override;
 };
 
+NS_END
