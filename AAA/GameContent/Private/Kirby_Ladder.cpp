2cf10752ae01ef35ffda2f35bdfac091238cd864
yoonseungeon <yoonseungeon@naver.com>
2026-07-21 21:10:17 +0900

    사다리 올라가는거 애니메이션 비율 적용, 사운드

--- AAA/GameContent/Private/Kirby_Ladder.cpp
+++ AAA/GameContent/Private/Kirby_Ladder.cpp
@@ -74,6 +74,7 @@ void CKirby_Ladder::Enter(CKirby* pKirby, _int iFlag)
     m_iCurMoveDir = 0;
     m_iPreMoveDir = 0;
     m_iRemainDownAniCells = m_iMaxDownAniCells;
+    m_fLadderAnimProgress = 0.f;
 
     m_eLadderState = LADDER_STATE::LADDER_END;
     Change_LadderState(pKirby, LADDER_STATE::WAIT);
@@ -81,6 +82,8 @@ void CKirby_Ladder::Enter(CKirby* pKirby, _int iFlag)
     CEffect_Loader::GetInstance()->Spawn(L"OnLadderEffect", pKirby->Get_LevelIndex(),
         _float3{ 0.f, -0.2f, 0.f }, _float3{ 0.f, 0.f, 0.f }, _float3{ 0.f, 0.f, 0.f },
         pKirby->Get_Transform()->Get_WorldMatrixPtr());
+
+    m_pGameInstance_Proxy->Play_SFX(L"HeroBasic_Ladder.wav", 0.5f);
 }
 
 void CKirby_Ladder::Update(CKirby* pKirby, const _float fTimeDelta)
@@ -190,7 +193,7 @@ void CKirby_Ladder::Enter_LadderState(CKirby* pKirby, LADDER_STATE eState)
             pKirby->Get_KirbyAbility()->Play_AbilityAni(pKirby, ABILITY_ANI::JUMP_END_L);
             CMovement_Child* pMovement = pKirby->Get_Movement();
             pMovement->Set_LinearDrag(CKirby::s_fLinearDrag);
-            pMovement->Force_Jump(3.f);
+            pMovement->Force_Jump(3.2f);
 
             break;
         }
@@ -217,13 +220,16 @@ void CKirby_Ladder::Update_LadderState(CKirby* pKirby, _float fTimeDelta)
         }
         case LADDER_STATE::MOVE:
         {
+            // 사다리 이동 중에 방향이 바뀌면
             if (m_iCurMoveDir != 0 && m_iPreMoveDir != m_iCurMoveDir)
             {
+                // 현재 위치와 목표 위치를 변경
                 std::swap(m_iCurLadderIndex, m_iNextLadderIndex);
                 m_iPreMoveDir = m_iCurMoveDir;
                 m_iCurMoveDir = 0;
             }
 
+            // 애니메이션 변경, 아래로 내려갈 때는 m_iRemainDownAniCells 소모해야 Ani 전환
             _bool bMoveDown = m_iNextLadderIndex < m_iCurLadderIndex;
             if(m_bPlayAniLadderUp && bMoveDown && m_iRemainDownAniCells <= 0)
                 Apply_LadderDown(pKirby);
@@ -249,40 +255,81 @@ void CKirby_Ladder::Update_LadderState(CKirby* pKirby, _float fTimeDelta)
                 const _float fCurrentY = XMVectorGetY(vCurPos);
                 const _float fTargetY = XMVectorGetY(vLadderNextCellPos);
 
-                const _float fRemainDistY = fabsf(fTargetY - fCurrentY);
-                const _float fPredictDistY = m_fLadderSpeed * fTimeDelta;
+                // Up Animation인 경우 사다리 올라간 비율에 맞게 Ani 설정
+                if (m_bPlayAniLadderUp)
+                {
+                    // 시작 셀의 위치를 가지고 온다.
+                    _vector vCurrentCellPos{};
+                    if (pLadder->Try_GetCellWorld(m_iCurLadderIndex, vCurrentCellPos))
+                    {
+                        const _float fStartY = XMVectorGetY(vCurrentCellPos);
+                        const _float fTotalDistance = fabsf(fTargetY - fStartY);
 
-                _bool bWillArrive = fPredictDistY >= fRemainDistY;
+                        if (fTotalDistance <= Helper::fEpsilon) MSG_BOX("Update Bug Point 3: CKirby_Ladder");
+
+                        // 비율을 구해서
+                        _float fMoveRatio = fabsf(fCurrentY - fStartY) / fTotalDistance;
+                        Helper::FloatClamp(fMoveRatio, 0.f, 1.f);
+
+                        // 애니메이션에 적용
+                        constexpr _float fMid = 0.5f;
+                        const _float fAnimProgress = fmodf(m_fLadderAnimProgress + fMoveRatio * fMid, 1.f);
+                        pKirby->Get_Body()->Get_Animator()->Seek(fAnimProgress);
+                    }
+                }
 
                 CMovement_Child* pMovement = pKirby->Get_Movement();
 
+                // 남은 거리보다 이동할 거리가 크면(도착)
+                const _float fRemainDistY = fabsf(fTargetY - fCurrentY);
+                const _float fPredictDistY = m_fLadderSpeed * fTimeDelta;
+
+                // 도착 정리
+                _bool bWillArrive = fPredictDistY >= fRemainDistY;
                 if (bWillArrive)
                 {
+                    if(m_bPlayAniLadderUp)
+                        m_pGameInstance_Proxy->Play_SFX(L"HeroBasic_Ladder.wav", 0.5f);
+
+                    // Snap 시키고
                     pTransform->Set_State(STATE::POSITION, vLadderNextCellPos);
                     pMovement->Sync_To_Controller();
 
+                    // 아래로 내려가는 중이였다면 Down Ani 전환을 위한 셀 개수 감소
                     if (bMoveDown)
                         --m_iRemainDownAniCells;
-                    else
+                    else // 올라가는 중이였으면 초기화
                         m_iRemainDownAniCells = m_iMaxDownAniCells;
 
+                    // 올라가는 중이었으면
+                    if (m_bPlayAniLadderUp)
+                    {
+                        // 진행 비율에 맞게 올라가는 애니메이션 세팅
+                        constexpr _float fMid = 0.5f;
+                        m_fLadderAnimProgress = fmodf(m_fLadderAnimProgress + fMid, 1.f);
+                        pKirby->Get_Body()->Get_Animator()->Seek(m_fLadderAnimProgress);
+                    }
+                    
                     m_iCurLadderIndex = m_iNextLadderIndex;
 
+                    // 최상단, 최하단 검사후 상태 전이 처리
                     if (Handle_LadderTopBottom(pKirby, pLadder))
                         return;
 
+                    // 입력이 더 없었으면 Wait
                     if(m_iCurMoveDir == 0)
                     {
                         m_iPreMoveDir = 0;
                         Change_LadderState(pKirby, LADDER_STATE::WAIT);
                     }
-                    else
+                    else // 있으면 다음 목표 세팅
                     {
                         Set_NextCell();
                     }
                     return;
                 }
 
+                // 속도 설정
                 _bool bMoveUp = m_iCurLadderIndex < m_iNextLadderIndex;
                 if (bMoveUp)
                     pMovement->Set_VelocityY(m_fLadderSpeed);
@@ -350,7 +397,10 @@ _bool CKirby_Ladder::Handle_LadderTopBottom(CKirby* pKirby, CLevelDesign_Ladder*
 void CKirby_Ladder::Apply_LadderUp(CKirby* pKirby)
 {
     CAnimator* pAnimator = pKirby->Get_Body()->Get_Animator();
-    pAnimator->Play("LadderUp", true, false, 0.1f, 1.5f);
+    // 애니 전환만하고
+    pAnimator->Play("LadderUp", true, false, 0.1f, 0.f);
+    // 비율에 맞게 세팅
+    pAnimator->Seek(m_fLadderAnimProgress);
     m_bPlayAniLadderUp = true;
     m_fLadderSpeed = 6.f;
 }
