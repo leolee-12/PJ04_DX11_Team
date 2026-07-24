9d8d3b81dd763c0bec26d9d185e8ff5150eeac5f
Marb1e0817 <mabll0817@gmail.com>
2026-07-24 09:35:52 +0900

    아레나 전용 로딩 추가

--- AAA/Launcher/Public/Level_Defines.h
+++ AAA/Launcher/Public/Level_Defines.h
@@ -5,5 +5,6 @@
 #include "Map_Loader.h"
 #include "Launcher_LevelProfiles.h"
 #include "Level_Loading.h"
+#include "Level_ArenaLoading.h"
 #include "GameContent_Events.h"
 #include "LevelLight_DB.h"
